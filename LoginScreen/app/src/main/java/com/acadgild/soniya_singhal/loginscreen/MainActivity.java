ea387cf0af8b42d939a0184a81eab42c4e2c3dd5
ssinghal16 <soniyasinghal1991@gmail.com>
2018-02-18 19:33:23 +0530

    Assignment and ScreenShot

---
+++ LoginScreen/app/src/main/java/com/acadgild/soniya_singhal/loginscreen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.acadgild.soniya_singhal.loginscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// Main Class
+public class MainActivity extends AppCompatActivity {
+
+    // On create Method
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
